0e9221de6ba6e8b778d0f3bb9779676a13ba09f7
dromelvan <dromelvan@aland.net>
2020-06-18 19:43:43 +0300

    D11MatchDays in D11LeagueDTO.

--- src/main/java/org/d11/rest/api/model/D11LeagueDTO.java
+++ src/main/java/org/d11/rest/api/model/D11LeagueDTO.java
@@ -1,8 +1,11 @@
 package org.d11.rest.api.model;
 
+import java.util.*;
+
 public class D11LeagueDTO extends LeagueBaseDTO {
 
     private SeasonBaseDTO season;
+    private List<Long> d11MatchDays = new ArrayList<Long>();
 
     public SeasonBaseDTO getSeason() {
         return season;
@@ -10,6 +13,14 @@ public class D11LeagueDTO extends LeagueBaseDTO {
 
     public void setSeason(SeasonBaseDTO season) {
         this.season = season;
+    }
+
+    public List<Long> getD11MatchDays() {
+        return d11MatchDays;
+    }
+
+    public void setD11MatchDays(List<Long> d11MatchDays) {
+        this.d11MatchDays = d11MatchDays;
     }
 
 }
