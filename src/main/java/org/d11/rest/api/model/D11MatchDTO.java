a6351ef6e527e01408a337a3c0eb33417cca4453
dromelvan <dromelvan@aland.net>
2020-06-20 23:39:21 +0300

    Added remaining player count to D11MatchDTO.

--- src/main/java/org/d11/rest/api/model/D11MatchDTO.java
+++ src/main/java/org/d11/rest/api/model/D11MatchDTO.java
@@ -1,10 +1,13 @@
 package org.d11.rest.api.model;
 
+import java.util.*;
+
 public class D11MatchDTO extends D11MatchBaseDTO {
 
     private D11TeamBaseDTO homeD11Team;
     private D11TeamBaseDTO awayD11Team;
     private D11MatchDayBaseDTO d11MatchDay;
+    private Map<Long, Long> remainingPlayerCount = new HashMap<>();
 
     public D11TeamBaseDTO getHomeD11Team() {
         return homeD11Team;
@@ -28,6 +31,14 @@ public class D11MatchDTO extends D11MatchBaseDTO {
 
     public void setD11MatchDay(D11MatchDayBaseDTO d11MatchDay) {
         this.d11MatchDay = d11MatchDay;
+    }
+
+    public Map<Long, Long> getRemainingPlayerCount() {
+        return remainingPlayerCount;
+    }
+
+    public void setRemainingPlayerCount(Map<Long, Long> remainingPlayerCount) {
+        this.remainingPlayerCount = remainingPlayerCount;
     }
 
 }
