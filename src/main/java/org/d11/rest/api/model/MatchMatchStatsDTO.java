b76a2943004646e9c77e58d15858428c87d937d1
dromelvan <dromelvan@aland.net>
2020-06-28 17:25:33 +0300

    MatchMatchStatsDTO uses PlayerMatchStatsByTeamIdPositionDTO.

--- src/main/java/org/d11/rest/api/model/MatchMatchStatsDTO.java
+++ src/main/java/org/d11/rest/api/model/MatchMatchStatsDTO.java
@@ -1,18 +1,14 @@
 package org.d11.rest.api.model;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 public class MatchMatchStatsDTO extends MatchMatchEventsDTO {
 
-    private Map<Long, Map<String, List<PlayerMatchStatDetailsDTO>>> playerMatchStats = new HashMap<>();
+    private PlayerMatchStatsByTeamIdPositionDTO playerMatchStats;
 
-    public Map<Long, Map<String, List<PlayerMatchStatDetailsDTO>>> getPlayerMatchStats() {
+    public PlayerMatchStatsByTeamIdPositionDTO getPlayerMatchStats() {
         return playerMatchStats;
     }
 
-    public void setPlayerMatchStats(Map<Long, Map<String, List<PlayerMatchStatDetailsDTO>>> playerMatchStats) {
+    public void setPlayerMatchStats(PlayerMatchStatsByTeamIdPositionDTO playerMatchStats) {
         this.playerMatchStats = playerMatchStats;
     }
 
